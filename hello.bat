98fd19e088d0b92d525aaa3bb0018bd52a5ef6ee
rupeshdubeyp <rupcscdubey@gmail.com>
2019-12-13 12:40:10 +0530

    commit up

--- hello.bat
+++ hello.bat
@@ -1,2 +1,81 @@
 echo "Hello World : %time%  : %date%"
 docker swarm join --token SWMTKN-1-60mlyhmfztajnwxgrywkg76jy0gcpxu5elm90ps7738n8fjxg1-3z7edoti8t5pbplx5f7yicosm 174.129.161.39:2377
+
+
+History
+ubuntu@ip-172-31-92-126:~$ history
+    1  docker pull alpine
+    2  docker run -it alpine /bin/sh
+    3  sudo apt -git update
+    4  sudo apt -get update
+    5  sudo apt-get update
+    6  \
+    7  sudo apt-get install docker.io
+    8  sudo systemctl status
+    9  sudo systemctl status docker
+   10  sudo usermod -aG docker ubuntu
+   11  sudosu-
+   12  sudo su-
+   13  sudo su -
+   14  git clone https://drive.google.com/drive/folders/1spz_bxLOic4Bit7-YNni72YAqtWaskSX?usp=sharing
+   15  git  clone https://github.com/rchidana/facebooc.git
+   16  ls
+   17  cd facebooc/
+   18  ls
+   19  docker  run -itd --name fb -p 8085:16000 ubuntu bash
+   20  docker ps
+   21  exit
+   22  ls
+   23  docker cp facebooc fb:/opt
+   24  docker attach fb
+   25  docker swarm init --advertise-addr 174.129.161.39
+   26  docker swarm join --token SWMTKN-1-1ee9nglgeawl4n069ey6b62ol6ynj9byzhjrd3an2bdqiwppmk-2w3batcsxwhzxm9gfh2mhlg2c 34.228.208.188:2377
+   27  docker nod els
+   28  docker nod ls
+   29  docker node ls
+   30  docker swarm leave
+   31  docker swarm join --token SWMTKN-1-1ee9nglgeawl4n069ey6b62ol6ynj9byzhjrd3an2bdqiwppmk-2w3batcsxwhzxm9gfh2mhlg2c 34.228.208.188:2377
+   32  'docker swarm join-token manager
+   33  docker swarm join-token manager
+   34  docker swarm ls
+   35  docker swarm leave
+   36  docker swarm leave --force
+   37* docker swarm leave
+   38  docker swarm join-token manager
+   39  docker service ls
+   40  docker service ps
+   41  docker swarm
+   42  docker swarm COMMAND --help
+   43  docker service ps
+   44  docker service ps wed
+   45  docker service scale web=5
+   46  docker ps
+   47  clear
+   48  docker ps
+   49  history
+   50  docker node ls
+   51  docker ps
+   52  docker swarm init --advertise-addr 174.129.161.39
+   53  docker swarm leave
+   54  docker swarm init --advertise-addr 174.129.161.39
+   55  docker service create --replicas 2 -p 80:80 --name web nginx
+   56  docker service lss
+   57  docker service ls
+   58  docker service ps web
+   59  docker service scale web=8
+   60  docker service ps
+   61  docker ps
+   62  docker service rm web
+   63  docker ps
+   64  docker swarm ls
+   65  docker swarm leave
+   66  docker swarm leave --force
+   67  docker swarm
+   68  docker node ls
+   69  docker swarm init --advertise-addr 174.129.161.39
+   70  docker service create --name vote --replicas 4 -p 8080:80 instavote/vote:movies
+   71  docker service update --image instavote/vote:indent
+   72  docker service update --image instavote/vote:indent vote
+   73  docker service update --image instavote/vote:latest vote
+   74  history
+
